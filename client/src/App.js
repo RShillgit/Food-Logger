2ae83c27d340263cf94dfa5d5f54f21e072b4752
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-05-09 11:44:48 -0400

    Basic meal modals

--- client/src/App.js
+++ client/src/App.js
@@ -42,20 +42,31 @@ function App(props) {
 
   }, [cookie])
 
-    // Anytime auth changes, set display
-    useEffect(() => {
+  // Anytime auth changes, set display
+  useEffect(() => {
 
-      // TODO: Loading
-      if (auth === null) {
-    
-      }
+    // TODO: Loading
+    if (auth === null) {
   
-      // Not Logged In redirect to login
-      else if (auth === false) {
-        navigate('/login')
-      }
-    }, [auth])
+    }
 
+    // Not Logged In redirect to login
+    else if (auth === false) {
+      navigate('/login')
+    }
+  }, [auth])
+
+  // Opens Meal Associated Modal
+  const openMealModal = (modalID) => {
+    const selectedModal = document.getElementById(modalID);
+    selectedModal.showModal();
+  }
+
+  // Closes Meal Assocaited Modal
+  const closeMealModal = (modalID) => {
+    const selectedModal = document.getElementById(modalID);
+    selectedModal.close();
+  }
 
   return (
     <div className="App mainPage">
@@ -63,25 +74,46 @@ function App(props) {
       {(auth)
         ?
         <div className='mainPage-container'>
+
           <div className='caloriesRemaining'>
             <h2>Calories Remaining: {caloriesRemaining}</h2>
           </div>
 
-          <div className='mealOverview'>
+          <div className='mealOverview' onClick={() => openMealModal('breakfastModal')}>
             <p>Breakfast {breakfastCalories}</p>
           </div>
+          <dialog id='breakfastModal'>
+            <button onClick={() => closeMealModal('breakfastModal')}>X</button>
+            <h1>Breakfast</h1>
+            <input type="text" placeholder="Searchbar" />
+            <p>Here we will show any existing breakfast foods</p>
+            <p>Egg Whites 65cals 120g</p>
+            <p>Bacon 150cals 3slices</p>
+          </dialog>
 
-          <div className='mealOverview'>
+          <div className='mealOverview' onClick={() => openMealModal('lunchModal')}>
             <p>Lunch {lunchCalories}</p>
           </div>
+          <dialog id='lunchModal'>
+            <button onClick={() => closeMealModal('lunchModal')}>X</button>
+            <h1>Lunch</h1>
+          </dialog>
 
-          <div className='mealOverview'>
+          <div className='mealOverview' onClick={() => openMealModal('dinnerModal')}>
             <p>Dinner {dinnerCalories}</p>
           </div>
+          <dialog id='dinnerModal'>
+            <button onClick={() => closeMealModal('dinnerModal')}>X</button>
+            <h1>Dinner</h1>
+          </dialog>
 
-          <div className='mealOverview'>
+          <div className='mealOverview' onClick={() => openMealModal('snackModal')}>
             <p>Snack {snackCalories}</p>
           </div>
+          <dialog id='snackModal'>
+            <button onClick={() => closeMealModal('snackModal')}>X</button>
+            <h1>Snack</h1>
+          </dialog>
 
           <div className='macroNutrient-stats'>
             <p>Carbs {carbCount}g</p>
