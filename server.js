2f07c9152c6bd0eb07b829f4d87ba5a6d1a93cc2
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-05-12 15:37:43 -0400

    CORS origin change

--- server.js
+++ server.js
@@ -53,15 +53,15 @@ app.use(passport.session());
  */
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000'})); // TODO: Change in deployment
+app.use(express.json()); 
+app.use(cors({origin: 'https://food-logger-production.up.railway.app'})); // http://localhost:3000
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // Prevent CORS Errors 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // TODO: Change in deployment
+  res.setHeader('Access-Control-Allow-Origin', 'https://food-logger-production.up.railway.app'); // http://localhost:3000
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', '*'); // X-Requested-With,content-type
   res.setHeader('Access-Control-Allow-Credentials', true);
